e69f28c9c50962503d03d4e7269071bf86ab9354
Shuheng Liu <wish1104@outlook.com>
2020-10-19 23:48:14 +0800

    feat(checker): aggregate checker warnings/errors instead of raising them

--- checker.py
+++ checker.py
@@ -1,11 +1,23 @@
 import re
 from abc import abstractmethod
-import warnings
 
 
 class Checker:
+    def __init__(self, summarizers=None):
+        if summarizers is None:
+            self.summarizers = []
+        else:
+            self.summarizers = list(summarizers)
+
+    def append_summarizer(self, s):
+        self.summarizers.append(s)
+
+    def update_summary(self, which, msg):
+        for s in self.summarizers:
+            s.register(self, which, msg)
+
     @abstractmethod
-    def check(self, s: str):
+    def check(self, filename, s: str):
         pass
 
 
@@ -14,11 +26,29 @@ MALE_PRONOUNS = ['he', 'him', 'his', 'himself']
 FEMALE_PRONOUNS = ['she', 'her', 'her', 'herself']
 
 
-class PlaceholderChecker:
-    def check(self, s: str):
+class CheckSummarizer:
+    def __init__(self):
+        self.check_class_names = []
+        self.which = []
+        self.messages = []
+
+    def register(self, checker, which, msg):
+        self.check_class_names.append(checker.__class__.__name__)
+        self.which.append(str(which))
+        self.messages.append(str(msg))
+
+    def get_summaries(self):
+        if len(self.check_class_names) == 0:
+            return None
+
+        return [f"{c}: in {w}: {m}" for c, w, m in zip(self.check_class_names, self.which, self.messages)]
+
+
+class PlaceholderChecker(Checker):
+    def check(self, filename, s: str):
         tags = re.findall(TAG_RE, s)
         if len(tags) != 0:
-            raise ValueError(f"unresolved tags found: {tags}, {s}")
+            self.update_summary(filename, f"unresolved tags found: {tags}, {s}")
 
         i = s.find("_")
         underscore_occurences = []
@@ -29,11 +59,11 @@ class PlaceholderChecker:
             i = s.find("_", i + 1)
 
         if len(underscore_occurences) != 0:
-            warnings.warn("resolved underscores: \n" + "\n".join(underscore_occurences))
+            self.update_summary(filename, "resolved underscores: \n" + "\n".join(underscore_occurences))
 
 
-class GenderChecker:
-    def check(self, s):
+class GenderChecker(Checker):
+    def check(self, filename, s):
         s = re.sub('[^A-Za-z0-9 ]', ' ', s)
         s = s.lower().split()
         male_indices = [i for i, w in enumerate(s) if w in MALE_PRONOUNS]
@@ -57,16 +87,18 @@ class GenderChecker:
                 check_indices = male_indices
                 confidence = len(female_indices) / (len(male_indices) + len(female_indices))
 
-            raise ValueError(f""" Both MALE and FEMALE pronouns found:
+            self.update_summary(
+                filename,
+                f""" Both MALE and FEMALE pronouns found:
                 Male pronoun count: {len(male_indices)}
                 Female pronoun count: {len(female_indices)}
                 Inferred gender is "{inferred_gender}" with confidence {confidence}
 
                 Check these occurrences of the opposite gender:
-                {windows(check_indices)}
-            """)
+                {windows(check_indices)}"""
+            )
         elif len(male_indices) == 0 and len(female_indices) == 0:
-            warnings.warn("No gender pronouns found")
+            self.update_summary(filename, "No gender pronouns found")
         else:
             if len(male_indices) > 0:
                 print("Inferred Gender: M")
@@ -74,30 +106,31 @@ class GenderChecker:
                 print("Inferred Gender: F")
 
 
-class NameChecker:
-    def __init__(self, first_names, last_names):
+class NameChecker(Checker):
+    def __init__(self, first_names, last_names, summarizers=None):
+        super(NameChecker, self).__init__(summarizers=summarizers)
         self.first_names = set(first_names)
         self.last_names = set(last_names)
 
-    def check(self, s, target_first_name, target_last_name):
+    def check(self, filename, s, target_first_name, target_last_name):
         s = re.sub('[^A-Za-z0-9 ]', ' ', s)
         s = s.split()
         old_s = " ".join(s)
         if target_first_name not in self.first_names:
-            warnings.warn(f"Target first name not listed in {self.first_names}: \n {old_s}")
+            self.update_summary(filename, f"Target first name not listed in {self.first_names}: \n {old_s}")
         if target_last_name not in self.last_names:
-            warnings.warn(f"Target last name not listed in {self.last_names}: \n {old_s}")
+            self.update_summary(filename, f"Target last name not listed in {self.last_names}: \n {old_s}")
 
         first_names = self.first_names - {target_first_name, target_last_name}
         last_names = self.last_names - {target_first_name, target_last_name}
 
         for fn, ln in zip(first_names, last_names):
             if fn in s:
-                warnings.warn(f'Alien first name "{fn}" found: \n {old_s}')
+                self.update_summary(filename, f'Alien first name "{fn}" found: \n {old_s}')
             if ln in s:
-                warnings.warn(f'Alien last name "{ln}" found: \n {old_s}')
+                self.update_summary(filename, f'Alien last name "{ln}" found: \n {old_s}')
 
         if not (target_first_name in s):
-            warnings.warn(f"Target first name not found: \n {old_s}")
+            self.update_summary(filename, f"Target first name not found: \n {old_s}")
         if not (target_last_name in s):
-            warnings.warn(f"Target last name not found: \n {old_s}")
+            self.update_summary(filename, f"Target last name not found: \n {old_s}")
