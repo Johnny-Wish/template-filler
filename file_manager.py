e4e440b5eb9141bac7d0c142ee7cef8bc6e36558
Shuheng Liu <wish1104@outlook.com>
2020-10-19 23:51:21 +0800

    feat(backend): implement a file sys manager for backend

---
+++ file_manager.py
@@ -0,0 +1,59 @@
+import os
+from fetcher import FlockFetcher, ProjectInfoFetcher, GenreFormer, StudentFetcher
+from controller import Controller
+from io_utils import safe_mkdir, extract_zip, zipdir, DocxInsertionWriter
+from global_utils import rreplace, get_time_str
+
+
+class FileSystemManager:
+    def __init__(self, zip_dir, extracted_dir, download_dir):
+        safe_mkdir(zip_dir)
+        safe_mkdir(extracted_dir)
+        safe_mkdir(download_dir)
+
+        self.ZIP_DIR = zip_dir
+        self.EXTRACTED_DIR = extracted_dir
+        self.DOWNLOAD_DIR = download_dir
+
+    def save_uploaded(self, file, filename):
+        uploaded_zip_path = os.path.join(self.ZIP_DIR, filename)
+        file.save(uploaded_zip_path)
+        return uploaded_zip_path
+
+    @staticmethod
+    def get_controller(project_root):
+        flock_fetcher = FlockFetcher(os.path.join(project_root, "flock"))
+        program_fetcher = ProjectInfoFetcher(os.path.join(project_root, "program_info"))
+        student_fetcher = StudentFetcher(root_dir=project_root, name_list_path="eval.csv", flock_fetcher=flock_fetcher)
+        former = GenreFormer(os.path.join(project_root, "genre"))
+        return Controller(genre_former=former, student_fetcher=student_fetcher, program_fetcher=program_fetcher)
+
+    @staticmethod
+    def run_controller(project_root, controller):
+        writer = DocxInsertionWriter(template_path=os.path.join(project_root, "style.docx"), pre_para_id=0)
+        letter_dir = os.path.join(project_root, "letters")
+        controller.write_to_disk(writer, output_dir=letter_dir)
+        return letter_dir
+
+    def handle(self, file, filename, check=True):
+        # save uploaded zip, and get the dir
+        filename = f"{get_time_str()}_{filename}"
+        uploaded_zip_path = self.save_uploaded(file, filename)
+
+        # extract zip to a new folder
+        extracted_path = os.path.join(self.EXTRACTED_DIR, rreplace(filename, ".zip", ""))
+        extract_zip(src=uploaded_zip_path, dest=extracted_path)
+
+        # instantiate a controller to handle the extracted folder
+        controller = self.get_controller(extracted_path)
+        if check:
+            controller.check_texts(output="raise")
+
+        # run the controller and generator docs, optionally gather error, info, debug
+        letter_dir = self.run_controller(extracted_path, controller)
+
+        # zip the docs folder and return download path
+        download_path = os.path.join(self.DOWNLOAD_DIR, filename)
+        zipdir(letter_dir, download_path)
+
+        return filename
